267386642bcf8782ab0f1e8cd5c0393cc8845d8c
Edison <ayoriachagua@gmail.com>
2023-05-21 21:09:37 -0500

    Agregar archivos de proyecto.

---
+++ IBattery/Interfaces/InBattery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBattery.Interfaces
+{
+    public enum BatteryStatus
+    {
+        Charging,
+        Discharging,
+        Full,
+        NotCharging,
+        Unknown
+    }
+
+    public enum PowerSource
+    {
+        Battery,
+        Ac,
+        Usb,
+        Wireless,
+        Other
+    }
+
+    public interface InBattery
+    {
+        int RemainingChargePercent { get; }
+        BatteryStatus Status { get; }
+        PowerSource PowerSource { get; }
+    }
+
+}
